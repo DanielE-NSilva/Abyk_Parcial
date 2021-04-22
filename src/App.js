2f4c59e9643e4d94942e5a7966c56d9a170d37f0
Juan-Velez <juan_velez82172@elpoli.edu.co>
2021-04-22 16:42:21 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ app.use(morgan('dev'));
 app.use(myConnection(mysql, {
     host: 'localhost',
     user: 'root',
-    password: 'ConstraseñaDeSuMysql',
+    password: 'juan999',
     port: 3306,
     database: 'NombreBaseDeDatos'
 }, 'single'));
