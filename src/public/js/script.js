a91152d3e42323556768dd5984c3b411a402e3a1
Juan-Velez <juan_velez82172@elpoli.edu.co>
2021-06-02 11:01:12 -0500

    mapa
    mapa inicio falta organizarlo

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,6 +1,17 @@
-
+$(function() {
 
  $("#TipoDeDocumento").val(document.getElementById("Control").value);
+
+ var map = L.map('mapa').setView([51.505, -0.09], 13);
+ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+ }).addTo(map);
+ L.marker([51.5, -0.09]).addTo(map)
+ .bindPopup('GDLWebCamp 2021')
+ .openPopup();
+
+
+});
   
  
 
